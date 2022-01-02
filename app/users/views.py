80fa768bf3605e5a99c06d81875e9160f2c1652b
Timizuo Ebideri <timizuoebideri@gmail.com>
2022-01-02 21:32:11 +0100

    Modify users.views add pagination feature to home route

--- app/users/views.py
+++ app/users/views.py
@@ -7,4 +7,8 @@ user_bp = Blueprint('users', __name__, template_folder='templates')
 
 @user_bp.route('/')
 def home():
-    return render_template('users/index.html')
+    page = request.args.get('page', default=1, type=int)
+    page_size = request.args.get('pagination', default=config("PAGE_SIZE", cast=int), type=int)
+    users = Users.query.order_by(Users._id).paginate(page=page, per_page=page_size)
+
+    return render_template('users/index.html', users=users, limit=page_size)
